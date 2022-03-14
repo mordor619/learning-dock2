c35ec3bd3a87b3438a4bbe6b01a4a7673fad0d0a
Manthan Sharma <manthansharma.bhanwar@kanini.com>
2022-03-14 17:45:59 +0530

    final

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learning_dock2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getvalues")]
+        public List<string> getvalues()
+        {
+            List<string> l = new List<string>();
+
+            l.Add("dock1");
+            l.Add("dock2");
+            l.Add("dock3");
+            l.Add("dock4");
+            l.Add("dock5");
+
+            return l;
+        }
+
+     }
+
+}
